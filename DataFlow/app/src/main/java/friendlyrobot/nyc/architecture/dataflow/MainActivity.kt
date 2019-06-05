d3c3445035a9d20a66acd74948c6360e0ba973cb
Brian Plummer <brian.r.plummer@gmail.com>
2019-06-05 13:14:48 -0400

    basic scaffolding

--- DataFlow/app/src/main/java/friendlyrobot/nyc/architecture/dataflow/MainActivity.kt
+++ DataFlow/app/src/main/java/friendlyrobot/nyc/architecture/dataflow/MainActivity.kt
@@ -2,11 +2,58 @@ package friendlyrobot.nyc.architecture.dataflow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+        val flows = arrayOf<DataFlow>(DataFlow(R.layout.view_friendly_layout, "FriendlyMVP"),
+            DataFlow(R.layout.view_friendly_layout, "FriendlyMVP2"),
+            DataFlow(R.layout.view_friendly_layout, "FriendlyMVP3"))
+
+        viewPager.adapter = DataFlowAdapter(flows)
+    }
+
+}
+
+data class DataFlow(
+    val resid: Int,
+    val label: String
+)
+
+class DataFlowAdapter(private val flows : Array<DataFlow>) : RecyclerView.Adapter<DataFlowViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataFlowViewHolder {
+        val inflatedView = LayoutInflater.from(parent.context).inflate(flows[viewType].resid, parent, false)
+        return DataFlowViewHolder(inflatedView)
+
+    }
+
+    override fun getItemCount(): Int {
+        return flows.size
+    }
+
+    override fun onBindViewHolder(holder: DataFlowViewHolder, position: Int) {
+
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
+}
+
+
+class DataFlowViewHolder internal constructor(private val dataFlowView: View) :
+        RecyclerView.ViewHolder(dataFlowView) {
+    internal fun bind() {
+        //do nthing currently
     }
 }
---
+++ DataFlow/friendlymvp/src/main/java/friendlyrobot/nyc/architecture/friendlymvp/FriendlyLayout.kt
@@ -0,0 +1,19 @@
+//package friendlyrobot.nyc.architecture.friendlymvp
+//
+//import android.content.Context
+//import android.util.AttributeSet
+//import androidx.constraintlayout.widget.ConstraintLayout
+//
+//class FriendlyLayout @JvmOverloads
+//constructor(
+//    context: Context,
+//    attrs: AttributeSet? = null,
+//    defStyle: Int = 0
+//) : ConstraintLayout(context, attrs, defStyle) {
+//
+//    init {
+//        inflate(context, R.layout.view_friendly_layout, this)
+//    }
+//
+//
+//}
